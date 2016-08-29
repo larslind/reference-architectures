7a8cfc49a7db36eb18b0a20039053a86e0909142
Andrew Oakley <anoakley@microsoft.com>
2016-08-29 11:16:25 -0700

    Add AD VMs

---
+++ guidance-iaas-ad/extensions/New-AzureVpnConnection.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+  [string]$InterfaceName = "Azure",
+  [string]$GatewayIpAddress,
+  [string]$VirtualNetworkAddressPrefix,
+  [string]$SharedKey
+)
+
+$ErrorActionPreference = "Stop"
+
+Add-VpnS2SInterface -Name $InterfaceName -Destination $GatewayIpAddress -Protocol IKEv2 -AuthenticationMethod PSKOnly -IPv4Subnet "$VirtualNetworkAddressPrefix`:2" -SharedSecret $SharedKey
+Connect-VpnS2SInterface -Name $InterfaceName
---
+++ guidance-iaas-ad/extensions/joindomain.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True)]
+  [string]$AdminUser,
+
+  [Parameter(Mandatory=$True)]
+  [string]$AdminPassword,
+
+  [Parameter(Mandatory=$True)]
+  [string]$DomainName
+)
+#  $AdminUser = "adminUser"
+#  $AdminPassword = "adminP@ssw0rd"
+#  $DomainName = "contoso.com"
+$secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("$DomainName\$AdminUser", $secAdminPassword)
+Add-Computer -DomainName $DomainName -Credential $credential
+Restart-Computer
