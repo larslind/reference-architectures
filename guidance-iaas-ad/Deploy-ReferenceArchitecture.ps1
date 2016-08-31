0c163ab71b4c5413cdb6de7e37a56069ea38245d
Andrew Oakley <anoakley@microsoft.com>
2016-08-31 15:06:50 -0700

    Start ADSF support

---
+++ guidance-iaas-ad/Deploy-ReferenceArchitecture.ps1
@@ -0,0 +1,51 @@
+﻿#
+# Deploy_ReferenceArchitecture.ps1
+#
+param(
+  [Parameter(Mandatory=$true)]
+  $SubscriptionId,
+  [Parameter(Mandatory=$false)]
+  $Location = "West US 2"
+)
+
+$ErrorActionPreference = "Stop"
+
+$templateRootUriString = $env:TEMPLATE_ROOT_URI
+if ($templateRootUriString -eq $null) {
+  $templateRootUriString = "https://raw.githubusercontent.com/mspnp/template-building-blocks/master/"
+}
+
+if (![System.Uri]::IsWellFormedUriString($templateRootUriString, [System.UriKind]::Absolute)) {
+  throw "Invalid value for TEMPLATE_ROOT_URI: $env:TEMPLATE_ROOT_URI"
+}
+
+Write-Host
+Write-Host "Using $templateRootUriString to locate templates"
+Write-Host
+
+$templateRootUri = New-Object System.Uri -ArgumentList @($templateRootUriString)
+# Template to configure AD for ADFS
+$virtualMachineExtensionsTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/virtualMachine-extensions/azuredeploy.json")
+$virtualMachineExtensionsParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\adfs\configure-ad-for-adfs.parameters.json")
+
+# ADFS Templates
+$loadBalancerTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/loadBalancer-backend-n-vm/azuredeploy.json")
+$loadBalancerParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\loadBalancer-adfs.parameters.json")
+
+$adResourceGroupName = "ra-ad-ad-rg"
+$adfsResourceGroupName = "ra-ad-adfs-rg"
+
+# Login to Azure and select your subscription
+Login-AzureRmAccount -SubscriptionId $SubscriptionId | Out-Null
+
+Write-Host "Configuring AD for ADFS..."
+New-AzureRmResourceGroupDeployment -Name "ra-ad-configure-ad-for-adfs-deployment" -ResourceGroupName $adResourceGroupName `
+    -TemplateUri $virtualMachineExtensionsTemplate.AbsoluteUri -TemplateParameterFile $virtualMachineExtensionsParametersFile
+
+# Create the resource group
+Write-Host "Creating ADFS resource group..."
+$adfsResourceGroup = New-AzureRmResourceGroup -Name $adfsResourceGroupName -Location $Location
+
+Write-Host "Deploying load balancer..."
+New-AzureRmResourceGroupDeployment -Name "ra-ad-adfs-deployment" -ResourceGroupName $adfsResourceGroup.ResourceGroupName `
+    -TemplateUri $loadBalancerTemplate.AbsoluteUri -TemplateParameterFile $loadBalancerParametersFile
