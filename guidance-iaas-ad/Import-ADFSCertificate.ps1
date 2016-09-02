520960836ded86c9027fb5cd8e90014db8a80145
Andrew Oakley <anoakley@microsoft.com>
2016-09-01 17:43:05 -0700

    Certificate work

---
+++ guidance-iaas-ad/Import-ADFSCertificate.ps1
@@ -0,0 +1,35 @@
+﻿Param(
+  [Parameter(Mandatory=$True)]
+  [string]$Base64EncodedCertificate,
+
+  [Parameter(Mandatory=$True)]
+  [string]$CertificatePassword,
+
+  [Parameter(Mandatory=$True)]
+  [ValidateSet("Cer", "Pfx")]
+  [string]$CertificateType,
+
+  [switch]$InstallTrustedRoot
+)
+
+$certificateFilename = "$PSScriptRoot\temp.$($CertificateType.ToLower())"
+[System.IO.File]::WriteAllBytes($certificateFilename, [System.Convert]::FromBase64String($Base64EncodedCertificate))
+$pwd = ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force
+
+try {
+    
+    switch ($CertificateType) {
+        "Cer" {
+            Import-Certificate -FilePath $certificateFilename -CertStoreLocation "Cert:\LocalMachine\My"
+        }
+        "Pfx" {
+            Import-PfxCertificate -FilePath $certificateFilename -CertStoreLocation "Cert:\LocalMachine\My" -Password $pwd
+            if ($InstallTrustedRoot) {
+                Import-PfxCertificate -FilePath $certificateFilename -CertStoreLocation "Cert:\LocalMachine\AuthRoot" -Password $pwd
+            }
+        }
+    }
+}
+finally {
+    Remove-Item -Path $certificateFilename
+}
---
+++ guidance-iaas-ad/extensions/Configure-AdfsServer.ps1
@@ -0,0 +1,51 @@
+﻿Param(
+  [Parameter(Mandatory=$True)]
+  [string]$AdminUser,
+
+  [Parameter(Mandatory=$True)]
+  [string]$AdminPassword,
+
+  [Parameter(Mandatory=$True)]
+  [string]$NetBiosDomainName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$FqDomainName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$GmsaName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$FederationName,
+
+  [Parameter(Mandatory=$True, ParameterSetName="InstallAdfsFarm")]
+  [string]$Description,
+
+  [Parameter(Mandatory=$True, ParameterSetName="AddAdfsFarmNode")]
+  [string]$PrimaryComputerName
+)
+
+$ErrorActionPreference = "Stop"
+
+$secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("$NetBiosDomainName\$AdminUser", $secAdminPassword)
+
+$thumbprint=(Get-ChildItem -DnsName $FederationName -Path cert:\LocalMachine\My).Thumbprint
+
+Install-WindowsFeature -IncludeManagementTools -Name ADFS-Federation
+Import-Module ADFS
+
+switch ($PsCmdlet.ParameterSetName) {
+    "InstallAdfsFarm" {
+        # Install a new ADFS farm
+        Install-AdfsFarm -CertificateThumbprint $thumbprint -FederationServiceDisplayName $Description -FederationServiceName $FederationName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaName`$" -Credential $credential -OverwriteConfiguration
+    }
+    "AddAdfsFarmNode" {
+        # Add a server to an existing ADFS farm
+        Add-AdfsFarmNode -CertificateThumbprint $thumbprint -PrimaryComputerName $PrimaryComputerName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaName`$" -Credential $credential
+    }
+}
+
+# Initialize device registration service for workplace join 
+Initialize-ADDeviceRegistration -ServiceAccountName "$NetBiosDomainName\$GmsaName`$" -DeviceLocation $FqDomainName -RegistrationQuota 10 -MaximumRegistrationInactivityPeriod 90 -Credential $Credential -Force
+
+Enable-AdfsDeviceRegistration -Credential $Credential -Force
