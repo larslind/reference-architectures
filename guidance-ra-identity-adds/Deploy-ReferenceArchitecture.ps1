f102fa6c363224216add620928550c63e420bdc7
Andrew Oakley <anoakley@microsoft.com>
2016-09-08 18:11:36 -0700

    Add AD RA

---
+++ guidance-ra-identity-adds/Deploy-ReferenceArchitecture.ps1
@@ -0,0 +1,26 @@
+﻿#
+# Deploy_ReferenceArchitecture.ps1
+#
+param(
+  [Parameter(Mandatory=$true)]
+  $SubscriptionId,
+  [Parameter(Mandatory=$false)]
+  $Location = "Central US"
+)
+
+$ErrorActionPreference = "Stop"
+
+$templateRootUriString = $env:TEMPLATE_ROOT_URI
+if ($templateRootUriString -eq $null) {
+  $templateRootUriString = "https://raw.githubusercontent.com/mspnp/template-building-blocks/master/"
+}
+
+if (![System.Uri]::IsWellFormedUriString($templateRootUriString, [System.UriKind]::Absolute)) {
+  throw "Invalid value for TEMPLATE_ROOT_URI: $env:TEMPLATE_ROOT_URI"
+}
+
+Write-Host
+Write-Host "Using $templateRootUriString to locate templates"
+Write-Host
+
+$templateRootUri = New-Object System.Uri -ArgumentList @($templateRootUriString)
