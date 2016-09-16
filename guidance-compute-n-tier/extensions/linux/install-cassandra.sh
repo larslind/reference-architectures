2d91591e746f830fddbaeb5d5ac47734fbfee2f4
Kirpa Singh <kirpas@microsoft.com>
2016-09-16 14:59:52 -0700

    Fixed issues with domain name label and passing location to scripts again

--- guidance-compute-n-tier/extensions/linux/install-cassandra.sh
+++ guidance-compute-n-tier/extensions/linux/install-cassandra.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 cloud_type="azure"
-seed_vm_name=$1
-opscenter_vm_name=$2
+seed_node_name=$1
+opscenter_node_name=$2
 data_center_name=$3
+location=$4
 
 echo "Input to install-cassandra.sh is:"
 echo cloud_type $cloud_type
-echo seed_vm_name $seed_vm_name
-echo opscenter_vm_name $opscenter_vm_name
+echo seed_node_name $seed_node_name
+echo opscenter_node_name $opscenter_node_name
 echo data_center_name $data_center_name
+echo location $location
 
-seed_node_dns_name="$seed_vm_name.cloudapp.azure.com"
-opscenter_dns_name="$opscenter_vm_name.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name.$location.cloudapp.azure.com"
+opscenter_dns_name="$opscenter_node_name.$location.cloudapp.azure.com"
 
 echo "Calling dse.sh with the settings:"
 echo cloud_type $cloud_type
--- guidance-compute-n-tier/extensions/linux/install-opscenter.sh
+++ guidance-compute-n-tier/extensions/linux/install-opscenter.sh
@@ -2,12 +2,14 @@
 
 cloud_type="azure"
 seed_node_name=$1
+location=$2
 
 echo "Input to node.sh is:"
 echo cloud_type $cloud_type
 echo seed_node_name $seed_node_name
+echo location $location
 
-seed_node_dns_name="$seed_node_name.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name.$location.cloudapp.azure.com"
 
 echo "Calling opscenter.sh with the settings:"
 echo cloud_type $cloud_type
