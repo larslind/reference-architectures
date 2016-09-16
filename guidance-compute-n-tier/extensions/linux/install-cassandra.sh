18b970d6ca43a1674a6c530bbd3986daa7d8396a
Kirpa Singh <kirpas@microsoft.com>
2016-09-16 11:45:22 -0700

    Simplified scripts and added domain name labels

--- guidance-compute-n-tier/extensions/linux/install-cassandra.sh
+++ guidance-compute-n-tier/extensions/linux/install-cassandra.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/env bash
 
 cloud_type="azure"
-seed_node_location=$1
-unique_string=$2
+seed_vm_name=$1
+opscenter_vm_name=$2
 data_center_name=$3
-opscenter_location=$4
-seed_vm_name=$5
-opscenter_vm_name=$6
 
 echo "Input to install-cassandra.sh is:"
 echo cloud_type $cloud_type
-echo seed_node_location $seed_node_location
-echo unique_string $unique_string
-echo opscenter_location $opscenter_location
+echo seed_vm_name $seed_vm_name
+echo opscenter_vm_name $opscenter_vm_name
+echo data_center_name $data_center_name
 
-seed_node_dns_name="$seed_vm_name$unique_string.$seed_node_location.cloudapp.azure.com"
-opscenter_dns_name="$opscenter_vm_name$unique_string.$opscenter_location.cloudapp.azure.com"
+seed_node_dns_name="$seed_vm_name.cloudapp.azure.com"
+opscenter_dns_name="$opscenter_vm_name.cloudapp.azure.com"
 
 echo "Calling dse.sh with the settings:"
 echo cloud_type $cloud_type
 echo seed_node_dns_name $seed_node_dns_name
 echo data_center_name $data_center_name
 echo opscenter_dns_name $opscenter_dns_name
-
-#echo dse_version $dse_version
 
 apt-get -y install unzip
 
--- guidance-compute-n-tier/extensions/linux/install-opscenter.sh
+++ guidance-compute-n-tier/extensions/linux/install-opscenter.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 cloud_type="azure"
-seed_node_location=$1
-unique_string=$2
-seed_node_name=$3
+seed_node_name=$1
 
 echo "Input to node.sh is:"
 echo cloud_type $cloud_type
-echo seed_node_location $seed_node_location
 echo seed_node_name $seed_node_name
-echo unique_string $unique_string
 
-seed_node_dns_name="$seed_node_name$unique_string.$seed_node_location.cloudapp.azure.com"
+seed_node_dns_name="$seed_node_name.cloudapp.azure.com"
 
 echo "Calling opscenter.sh with the settings:"
 echo cloud_type $cloud_type
